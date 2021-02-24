da69ee54bdd2a6b448f31a2504e1a3ae05d750bf
Alexandro castro <alexandro.castro@conexasaude.com.br>
2021-02-24 00:03:52 -0300

    add ORM

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
+import 'reflect-metadata'
 import express from 'express';
+import './database'
+import { router } from './router';
 
 const app = express();
+app.use(express.json())
+app.use(router)
 
 app.get('/', (req, res) => {
     return res.json({status: 200});
